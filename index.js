6e5a496effce2b90a9e7c9a8207a6ad8943afe3c
josetoba4 <jcarlostobiasb@gmail.com>
2025-02-27 22:23:43 -0600

    upload 1st part

---
+++ index.js
@@ -0,0 +1,76 @@
+var var1="aa"
+
+function hello(){
+    console.log();
+}
+
+// function repeat()=>{
+    
+// }
+
+// repeat(hello,5){
+
+// }
+
+//////////////////////////Repaso///////////////////////////////
+
+// var nom = "Jose"
+
+// let prenom = "Hernandez"
+
+// const message = "Bienvenue"
+
+// console.log(`${message} ${nom} ${prenom}`);
+
+// x=10
+
+// const functionAnonyme = function (x) {
+//     return x+x
+// }
+
+// // console.log(functionAnonyme(x)); 
+
+// // console.log((x) => {return x*x});
+
+
+// const table = [ 1,2,3,4,5 ] 
+
+// table.forEach(element => {
+//     console.log(element);
+// });
+
+// table.map(element => {
+//     console.log(element * 2); 
+// })
+
+// table.filter(element => {
+//     console.log( element % 2 === 0);
+// })
+
+// table
+//     .map(element => element*2)
+//     .filter(element => element/2)
+//     .forEach(element => console.log(element))
+
+// const [premier, _, seconde, ...rest] = table
+
+// const object = {
+//     premier:10,
+//     seconde:20
+// }
+
+// let {premier: premierP, seconde: secondeP} = object
+
+// console.log(object);
+
+// var body = document.getElementsByName('body')
+
+// var button = document.getElementById('button')
+
+// body.innerHTML(
+//     <div>
+//         Bienvenue
+//         {{nom}}
+//         {{prenom}}
+//     </div>
+//     );
